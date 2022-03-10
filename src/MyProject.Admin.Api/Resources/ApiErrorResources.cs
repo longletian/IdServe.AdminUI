223ee76f574ad7d486a294fb9291b6664102588d
letian long <longletian@outlook.com>
2022-03-10 23:24:19 +0800

    添加项目文件。

---
+++ src/MyProject.Admin.Api/Resources/ApiErrorResources.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Jan Škoruba. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+
+using MyProject.Admin.Api.ExceptionHandling;
+
+namespace MyProject.Admin.Api.Resources
+{
+    public class ApiErrorResources : IApiErrorResources
+    {
+        public virtual ApiError CannotSetId()
+        {
+            return new ApiError
+            {
+                Code = nameof(CannotSetId),
+                Description = ApiErrorResource.CannotSetId
+            };
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ src/MyProject.Admin.Api/Resources/IApiErrorResources.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Jan Škoruba. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+
+using MyProject.Admin.Api.ExceptionHandling;
+
+namespace MyProject.Admin.Api.Resources
+{
+    public interface IApiErrorResources
+    {
+        ApiError CannotSetId();
+    }
+}
+
+
+
+
+
+
+
